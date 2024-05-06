d90daa294645f8d50e451f47cea656b20053adf7
Rohit Gautam <115552308+rohitgautam16@users.noreply.github.com>
2024-05-06 18:46:52 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = ("12345")
+let accountmail = "rg12345@google.com"
+var accountpassword = "password"
+accountCity = "Delhi"
+let accountState
+
+// accountId = "23456" ( Not Allowed)
+
+accountmail = "hchc12@gmail.com"
+accountpassword = "hospital"
+accountCity = "Bengaluru"
+
+/*
+Prefer not to use var
+because of issue in block scope or functional scope
+
+*/
+
+console.table([accountId , accountmail, accountpassword, accountCity, accountState])
---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) we are using nodejs not browsers //
+
+console.log(3
+    +3) // code readability should be high
+
+console.log("rohit")
+
+let name = "Rohit"
+let age = 21
+let IsloggedIn = true
+let state 
+
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false 
+// null => standalone value
+// undefned => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
