0f52510db5c9b56854cd982c314fac379fe5c3ac
Rohit Gautam <115552308+rohitgautam16@users.noreply.github.com>
2024-05-07 19:57:20 +0000

    conversion of data types

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,31 @@
+let score = undefined
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true  => 1; false => 0
+
+let isloggedIn = "rohit"
+
+let booleanisloggedIn = Boolean(isloggedIn)
+
+// console.log(booleanisloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "rohit" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
