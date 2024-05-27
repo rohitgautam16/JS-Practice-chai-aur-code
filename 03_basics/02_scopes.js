2c4785d0435a2939830d47c3b692ee62793cf7d9
Rohit Gautam <paarthsingh13@gmail.com>
2024-05-27 19:27:14 +0000

    scopes and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,11 +3,48 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("Inner :", a);
+    // console.log("Inner :", a);
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Rohit"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+
+}
+
+// one()
+
+if(true){
+    const username = "Rohit"
+    if(username === "Rohit"){
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website); // not is scope
+}
+// console.log(username); // not in scope
+
+// ++++++++++++++++++++++++++++++ Intresting ++++++++++++++++++++++++++
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
